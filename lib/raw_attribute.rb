eda696530c04a9b75f4895e0e0469caf718aa769
Hemant Kumar <hkumar@crri.co.in>
2011-01-03 12:50:14 +0530

    fix stfuff

--- lib/raw_attribute.rb
+++ lib/raw_attribute.rb
@@ -1,33 +1,35 @@
-class ActiveRecord::Base
+module RawAttribute
+  extend ActiveSupport::Concern
   IGNORE_ATTRIBUTES = ["id", "created_at", "updated_at"]
+  class RawAttributeInvalidColumnType < Exception; end
+  module ClassMethods
+    def raw_attribute(*args)
+      options = args.extract_options!
 
-  def self.raw_attribute(*args)
-    options = args.extract_options!
+      case args.first
+        when :all then
+          attrs = self.new.attribute_names
+        else
+          attrs = args
+      end
 
-    case args.first
-      when :all then attrs = self.new.attribute_names
-      else           attrs = args
-    end
+      attrs -= options[:except] if options.has_key? :except
+      attrs -= IGNORE_ATTRIBUTES
 
-    attrs -= options[:except] if options.has_key? :except
-    attrs -= IGNORE_ATTRIBUTES
-
-    attrs.each do |a|
-      class_eval <<-STR, __FILE__,__LINE__ + 1
-        def #{a}
-          read_attribute('#{a}'.to_sym).to_s.html_safe
+      attrs.each do |a|
+        if self.columns_hash[a.to_s].text?
+          class_eval <<-STR, __FILE__, __LINE__ + 1
+          def #{a}
+            read_attribute('#{a}'.to_sym).to_s.html_safe
+          end
+          STR
+        else
+          raise RawAttributeInvalidColumnType.new("Cant use columns other than text or strings as raw, invalid column type #{a}")
         end
-      STR
+      end
     end
-  end
+
+  end # end of module#ClassMethods
 end
 
-
-module RawHtml
-  class Base
-    def self.raw(str)
-      str.to_s.html_safe
-    end
-  end
-end
-
+ActiveRecord::Base.send(:include,RawAttribute)
